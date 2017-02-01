d253946e6b9074b46d445866e0a3195dd5601f2a
k_fukuyori <ocvxkeikei@gmail.com>
2017-02-01 13:05:11 +0000

    test

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -5,4 +5,18 @@ class AdminController < ApplicationController
     end
   end
   
+  def user_lock
+        @user = User.find(params[:id])
+        @user.lock_access!
+        flash[:notice] = @user.email + "をロックしました。"
+        redirect_to action: 'index'
+  end
+
+  def user_unlock
+        @user = User.find(params[:id])
+        @user.unlock_access!
+        flash[:notice] = @user.email + "のロックを解除しました。"
+        redirect_to admin_index_path
+  end
+  
 end
